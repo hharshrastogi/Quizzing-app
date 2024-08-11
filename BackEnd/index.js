62d43ebec77cf4856fe65c1644c7912993152cf3
Harsh Rastogi <hharshrastogi@gmail.com>
2024-08-12 01:36:37 +0530

    Connected to online database

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,1 +1,39 @@
-console.log("HELLO");
+const { PrismaClient } = require('@prisma/client')
+const express = require('express');
+
+const app = express();
+
+const prisma = new PrismaClient()
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+console.log(`Server is running on port ${port}`);
+});
+
+
+
+app.get('/v1/flash_cards', async (req,res)=> {
+        try{
+         
+        }
+        catch (e){
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+        }
+  
+
+})
+
+
+app.post('/v1/flash_cards', async (req, res) => {
+try {
+
+} catch (e) {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+}
+})
