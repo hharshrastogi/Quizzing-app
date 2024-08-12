03cd44efcfc9e3b2fe7134ec0172b49e49cfb37c
Harsh Rastogi <hharshrastogi@gmail.com>
2024-08-12 22:49:37 +0530

    deploy error changes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cors({
-    origin:"https://quizzing-app.onrender.com",
+    origin:"https://quizzing-app-frontend.onrender.com"
 }))
 const prisma = new PrismaClient()
 
